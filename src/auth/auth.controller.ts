6415cef9f6758d42f19ad36fc27f9cac7048814e
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-05-30 22:33:07 -0400

    add create user functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDTO } from './dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService) {}
 
-  @Post('login')
-  login() {
-    return this.authService.login();
-  }
+    @Post('login')
+    login() {
+      return this.authService.login();
+    }
 
-  @Post('register')
-  register() {
-    return this.authService.register();
-  }
+    @Post('register')
+    register(@Body() dto: AuthDTO) {
+      return this.authService.register(dto);
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, Req } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDTO } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  login() {
+  constructor(private prisma: PrismaService) {}
+  login(){
+    // return the saved user
     return { message: 'I am logged in.' };
   }
-  register() {
-    return 'I am registered.';
+  async register(dto: AuthDTO) {
+    try {
+      // get hashed password 
+      const hash = await argon.hash(dto.password);
+      // save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash
+        }
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('User already exists.');
+        }
+      }
+      throw error;
+    }
   }
 }
